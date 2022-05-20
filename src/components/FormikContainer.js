de48afd0020164498391c505fc439814dae55362
thehasanovv <hasanov.e95@gmail.com>
2022-05-20 16:19:29 +0400

    update

--- src/components/FormikContainer.js
+++ src/components/FormikContainer.js
@@ -42,78 +42,82 @@ const FormikContainer = () => {
   };
 
   return (
-    <Formik
-      initialValues={initialValues}
-      validationSchema={validationSchema}
-      onSubmit={onSubmit}
-    >
-      {(formik) => (
-        <Form className="form">
-          <FormikControl
-            control="input"
-            type="text"
-            label="Name"
-            name="name"
-            variant="filled"
-          />
-          <FormikControl
-            control="input"
-            type="text"
-            label="Surname"
-            name="surname"
-            variant="filled"
-          />
-          <FormikControl
-            control="input"
-            type="email"
-            label="Email"
-            name="email"
-            variant="filled"
-          />
-          <FormikControl
-            control="input"
-            type="number"
-            label="Number"
-            name="number"
-            variant="filled"
-          />
-          <FormikControl
-            control="input"
-            type="password"
-            label="Password"
-            name="password"
-            variant="filled"
-          />
-          <FormikControl
-            control="input"
-            type="password"
-            label="Confirm password"
-            name="passwordConfirmation"
-            variant="filled"
-          />
-          <FormikControl
-            control="date"
-            label="Birth year"
-            name="birthDate"
-            variant="filled"
-          />
-          <FormikControl
-            control="radio"
-            label="Gender"
-            name="radioOption"
-            variant="filled"
-            options={radioOptions}
-          />
-          <button
-            type="submit"
-            className="registerButton"
-            disabled={!formik.isValid || formik.isSubmitting}
-          >
-            Register
-          </button>
-        </Form>
-      )}
-    </Formik>
+    <div className="container">
+      <div className="wrapper">
+        <Formik
+          initialValues={initialValues}
+          validationSchema={validationSchema}
+          onSubmit={onSubmit}
+        >
+          {(formik) => (
+            <Form className="form">
+              <FormikControl
+                control="input"
+                type="text"
+                label="Name"
+                name="name"
+                variant="filled"
+              />
+              <FormikControl
+                control="input"
+                type="text"
+                label="Surname"
+                name="surname"
+                variant="filled"
+              />
+              <FormikControl
+                control="input"
+                type="email"
+                label="Email"
+                name="email"
+                variant="filled"
+              />
+              <FormikControl
+                control="input"
+                type="number"
+                label="Number"
+                name="number"
+                variant="filled"
+              />
+              <FormikControl
+                control="input"
+                type="password"
+                label="Password"
+                name="password"
+                variant="filled"
+              />
+              <FormikControl
+                control="input"
+                type="password"
+                label="Confirm password"
+                name="passwordConfirmation"
+                variant="filled"
+              />
+              <FormikControl
+                control="date"
+                label="Birth year"
+                name="birthDate"
+                variant="filled"
+              />
+              <FormikControl
+                control="radio"
+                label="Gender"
+                name="radioOption"
+                variant="filled"
+                options={radioOptions}
+              />
+              <button
+                type="submit"
+                className="registerButton"
+                disabled={!formik.isValid || formik.isSubmitting}
+              >
+                Register
+              </button>
+            </Form>
+          )}
+        </Formik>
+      </div>
+    </div>
   );
 };
 
